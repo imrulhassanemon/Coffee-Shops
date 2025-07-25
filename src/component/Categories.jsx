66cdf32b129b7fdc33a8068fdc238dd36ab93d8f
Imrul Hassaan Emon <imrulhassan989@gmail.com>
2025-07-25 13:00:11 +0600

    get coffee by category

--- src/component/Categories.jsx
+++ src/component/Categories.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import { Link, } from 'react-router-dom';
 
 const Categories = ({categories}) => {
-  console.log(categories);
 
  
 
     return (
         <div role="tablist" className="tabs tabs-lift w-full my-9 flex justify-around">
        {
-        categories.map(category => (<Link key={category.category} className='tab ' role='tab' to={`/category/:${category.category}`}>{category.category}</Link>))
+        categories.map(category => (<Link key={category.category} className='tab ' role='tab' to={`/category/${category.category}`}>{category.category}</Link>))
        }
       </div>
     );
--- src/component/CoffeeCards.jsx
+++ src/component/CoffeeCards.jsx
@@ -1,22 +1,25 @@
-import React from 'react';
-import { useLoaderData, useParams } from 'react-router-dom';
-import Card from './Card';
-
+import React, { useEffect } from "react";
+import { useLoaderData, useParams } from "react-router-dom";
+import Card from "./Card";
 const CoffeeCards = () => {
+  const {category}  = useParams();
+  const data = useLoaderData();
 
-     const obj =  useParams()
-  console.log(obj);
+  const [coffees, setCoffees] = React.useState([]);//category
+  useEffect(() => {
+    const filteredByCategory = [...data].filter(coffee => coffee.category === category);
+    setCoffees(filteredByCategory);
+  }, [data, category]);
 
-  const coffees = useLoaderData();
   console.log(coffees)
 
-    return (
-        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4'>
-            {
-                coffees.map(coffee => <Card key={coffee.category} coffee={coffee}></Card>)
-            }
-        </div>
-    );
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+      {coffees.map((coffee) => (
+        <Card key={coffee.id} coffee={coffee}></Card>
+      ))}
+    </div>
+  );
 };
 
 export default CoffeeCards;
